707f1efa3237fc0c80c8a55993cd067fc79d3fde
Kevin Hoang <kevin.hoang322@gmail.com>
2025-12-19 11:16:50 -0600

    iframe adjustments - allow encrypted media

--- app/pages/youtube/[youtubeId].vue
+++ app/pages/youtube/[youtubeId].vue
@@ -1,10 +1,3 @@
-<script lang="ts" setup>
-const youtubeId = useRoute().params.youtubeId;
-const embedUrl = `https://www.youtube-nocookie.com/embed/${youtubeId}?rel=0`;
-// example video: https://www.youtube-nocookie.com/embed/BeyQ4hPDPUI
-// example video with wrong audio track: https://www.youtube-nocookie.com/embed/KVT_ODZn9Ys
-</script>
-
 <template>
   <div>
     <iframe
@@ -14,9 +7,17 @@ const embedUrl = `https://www.youtube-nocookie.com/embed/${youtubeId}?rel=0`;
       allowfullscreen
       frameborder="0"
       className="video"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     >
     </iframe>
   </div>
 </template>
+
+<script lang="ts" setup>
+const youtubeId = useRoute().params.youtubeId;
+const embedUrl = `https://www.youtube.com/embed/${youtubeId}?rel=0`;
+// example video: https://www.youtube-nocookie.com/embed/BeyQ4hPDPUI
+// example video with wrong audio track: https://www.youtube-nocookie.com/embed/KVT_ODZn9Ys
+</script>
 
 <style></style>
